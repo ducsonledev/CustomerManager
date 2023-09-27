124050ef5e2f041b28c319c98f6fc589131373e0
Son <72577766+ducsonledev@users.noreply.github.com>
2023-09-27 23:00:18 +0200

    fix imports

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-//import UserProfile from "./UserProfile.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
