23887086bea8b771cec04de1e61ca941b657b7d1
Son <72577766+ducsonledev@users.noreply.github.com>
2023-09-28 16:02:16 +0200

    using children props. creating test user objects

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,18 +1,44 @@
 import UserProfile from "./UserProfile.jsx";
 
+const user = [
+    {
+        name: "Jamila",
+        age: 22,
+        gender: "FEMALE"
+    },
+    {
+        name: "Anne",
+        age: 41,
+        gender: "FEMALE"
+    },
+    {
+        name: "Rex",
+        age: 22,
+        gender: "MALE"
+    },
+    {
+        name: "Moos",
+        age: 22,
+        gender: "IT"
+    }
+]
+
+
 function App() {
     const name = "John";
     return <div>
                 <UserProfile
                     name={"John"}
                     age={22}
-                    gender={"men"}
-                />
+                    gender={"men"}>
+                    <p>Hello</p>
+                </UserProfile>
                 <UserProfile
                     name={"Maria"}
                     age={23}
-                    gender={"women"}
-                />
+                    gender={"women"}>
+                    <h1>Ciao</h1>
+                </UserProfile>
                 <p>test {name} </p>
                 <h1>hello {name}</h1>
            </div>
--- frontend/react/src/UserProfile.jsx
+++ frontend/react/src/UserProfile.jsx
@@ -1,9 +1,10 @@
-const UserProfile = ({name, age, gender}) => {
+const UserProfile = ({name, age, gender, ...props}) => {
     return <div>
                 <h1>{name}</h1>
                 <p>{age}</p>
                 <img src={`https://randomuser.me/api/portraits/${gender}/75.jpg`} />
                 <p>UserProfile</p>
+                {props.children}
            </div>
 }
 
