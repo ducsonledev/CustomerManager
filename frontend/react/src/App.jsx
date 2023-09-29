64551bd81770d8d6b9e2d05c430291c82ef322b7
Son <72577766+ducsonledev@users.noreply.github.com>
2023-09-29 16:59:05 +0200

    issue CORS
    Cross-Origin Resource Sharing (CORS)

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,7 +1,18 @@
 import { Button } from '@chakra-ui/react';
 import SideBarWithHeader from "./shared/SideBar.jsx";
+import {useEffect} from 'react';
+import {getCustomers} from "./services/client.js";
 
 const App = () => {
+
+    useEffect(() => {
+        getCustomers().then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    },[])
+
     return (
         <SideBarWithHeader>
             <Button colorScheme='teal' variant='outline'>Click me</Button>
