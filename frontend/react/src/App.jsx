077a0cfe16e473b9a4643728802645da64f621be
Son <72577766+ducsonledev@users.noreply.github.com>
2023-09-29 19:43:43 +0200

    GET customer names from our db and show  in UI

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,21 +1,51 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Spinner, Text } from '@chakra-ui/react';
 import SideBarWithHeader from "./shared/SideBar.jsx";
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {getCustomers} from "./services/client.js";
 
 const App = () => {
 
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
+        setLoading(true);
         getCustomers().then(res => {
-            console.log(res)
+            setCustomers(res.data)
         }).catch(err => {
             console.log(err)
-        })
-    },[])
+        }).finally(() => {
+            setLoading(false)
+        }
+    )},[])
+
+    if (loading) {
+        return (
+            <SideBarWithHeader>
+                <Spinner
+                  thickness='4px'
+                  speed='0.65s'
+                  emptyColor='gray.200'
+                  color='blue.500'
+                  size='xl'
+                />
+            </SideBarWithHeader>
+        )
+    }
+
+    if(customers.length <= 0) {
+        return (
+            <SideBarWithHeader>
+                <Text>No customers available!</Text>
+            </SideBarWithHeader>
+        )
+    }
 
     return (
         <SideBarWithHeader>
-            <Button colorScheme='teal' variant='outline'>Click me</Button>
+            {customers.map((customer, index) => (
+                <p>{customer.name}</p>
+            ))}
         </SideBarWithHeader>
     )
 }
