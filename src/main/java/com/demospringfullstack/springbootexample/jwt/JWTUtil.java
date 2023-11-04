2e1dfceffbb6ee2bae0acd87c1ffa1aab81fe681
Son <72577766+ducsonledev@users.noreply.github.com>
2023-11-04 19:10:25 +0100

    creating jwt token

---
+++ src/main/java/com/demospringfullstack/springbootexample/jwt/JWTUtil.java
@@ -0,0 +1,45 @@
+package com.demospringfullstack.springbootexample.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JWTUtil {
+
+    private static final String SECRET_KEY =
+            "foobar_123456789_foobar_123456789_foobar_123456789_foobar_123456789";
+
+    public String issueToken(String subject) {
+        return issueToken(subject, Map.of());
+    }
+
+    public String issueToken(String subject, String ...scopes) {
+        return issueToken(subject, Map.of("scopes", scopes));
+    }
+
+    public String issueToken(String subject, Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuer("http://localhost")
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now()
+                        .plus(15, ChronoUnit.DAYS)
+                ))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+}
