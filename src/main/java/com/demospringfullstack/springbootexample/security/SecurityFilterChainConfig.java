953c2ae840549bc29db40d199ab995ff6ec642a9
Son <72577766+ducsonledev@users.noreply.github.com>
2023-11-09 14:48:35 +0100

    fix cors issue, custom cors from cors spring  security documentation

--- src/main/java/com/demospringfullstack/springbootexample/security/SecurityFilterChainConfig.java
+++ src/main/java/com/demospringfullstack/springbootexample/security/SecurityFilterChainConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,6 +32,7 @@ public class SecurityFilterChainConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/api/v1/customers")
                 .permitAll()
