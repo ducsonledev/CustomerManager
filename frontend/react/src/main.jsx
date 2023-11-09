13ad3a6365f6ee6e2abb303e397bb5b20d254aa8
Son <72577766+ducsonledev@users.noreply.github.com>
2023-11-09 23:49:37 +0100

    login page with chakra ui

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createStandaloneToast } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/login/Login.jsx'
 import './index.css'
 
-
 const { ToastContainer } = createStandaloneToast()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login></Login>
+  },
+  {
+    path: "dashboard",
+    element: <App></App>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <ChakraProvider>
-        <App />
+        <RouterProvider router={router} />
         <ToastContainer></ToastContainer>
       </ChakraProvider>
   </React.StrictMode>,
