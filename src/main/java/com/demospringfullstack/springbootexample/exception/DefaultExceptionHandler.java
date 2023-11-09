af474a3e639696571ddd6284e715714affd1c9f2
Son <72577766+ducsonledev@users.noreply.github.com>
2023-11-09 12:15:55 +0100

    fix authentication exceptions

--- src/main/java/com/demospringfullstack/springbootexample/exception/DefaultExceptionHandler.java
+++ src/main/java/com/demospringfullstack/springbootexample/exception/DefaultExceptionHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,9 +20,49 @@ public class DefaultExceptionHandler {
         ApiError apiError = new ApiError(
                 request.getRequestURI(),
                 e.getMessage(),
-                response.getStatus(),
+                HttpStatus.NOT_FOUND.value(),
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicateResourceException.class)
+    public ResponseEntity<ApiError> handleException(DuplicateResourceException e,
+                                                    HttpServletRequest request,
+                                                    HttpServletResponse response) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.CONFLICT.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    public ResponseEntity<ApiError> handleException(InsufficientAuthenticationException e,
+                                                    HttpServletRequest request,
+                                                    HttpServletResponse response) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.FORBIDDEN.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.FORBIDDEN);
+    }
+
+    //general exception handler, if thrown add new exception handler
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception e,
+                                                    HttpServletRequest request,
+                                                    HttpServletResponse response) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
