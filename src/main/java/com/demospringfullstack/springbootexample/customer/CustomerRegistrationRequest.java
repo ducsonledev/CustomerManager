3a7840c6e3d96e68705ec0639f7cc647be228aa1
Son <72577766+ducsonledev@users.noreply.github.com>
2024-03-31 11:32:26 +0200

    fixed not null validation

--- src/main/java/com/demospringfullstack/springbootexample/customer/CustomerRegistrationRequest.java
+++ src/main/java/com/demospringfullstack/springbootexample/customer/CustomerRegistrationRequest.java
@@ -3,12 +3,19 @@ package com.demospringfullstack.springbootexample.customer;
 import com.demospringfullstack.springbootexample.enums.Gender;
 import com.demospringfullstack.springbootexample.enums.Role;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CustomerRegistrationRequest(
+                @NotBlank
                 String name,
+                @NotBlank
                 String email,
+                @NotBlank
                 String password,
+                @NotNull
                 Integer age,
+                @NotNull
                 Gender gender,
                 @Nullable
                 Role role
--- src/main/java/com/demospringfullstack/springbootexample/customer/CustomerUpdateRequest.java
+++ src/main/java/com/demospringfullstack/springbootexample/customer/CustomerUpdateRequest.java
@@ -1,11 +1,16 @@
 package com.demospringfullstack.springbootexample.customer;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CustomerUpdateRequest (
+        @NotBlank
         String name,
+        @NotBlank
         @Email(message = "Requires valid email address")
         String email,
+        @NotNull
         Integer age
 ) {
 }
